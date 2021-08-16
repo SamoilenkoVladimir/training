dc6550a57c1123388c7f46c236c38ed34042de69
Vladimir Samoilenko <81811148+SamoilenkoVladimir@users.noreply.github.com>
2021-08-16 11:57:08 +0300

    Create Replacement.java

---
+++ task2-strings/src/Replacement.java
@@ -0,0 +1,24 @@
+
+
+public class Replacement {
+	
+	public static String wordReplace(String arrString,String r,Integer length) {
+		//замена слов заданной длины на последовательность символов (для этого строка разбивается на слова)
+		arrString=" "+arrString+" ";
+		r=" "+r+" ";
+		String newString[]=arrString.split(" ");
+		arrString="";
+        for (String string : newString) {
+        	string=" "+string+" ";
+            string=string.replaceAll(String.format("\\s\\S{%d}\\s+", length), r);
+            arrString=arrString+string;
+           // System.out.println("after ="+string);
+        }
+        arrString=" "+arrString+" ";
+        arrString = arrString.replaceAll("\\p{Blank}+", " ");
+		
+		
+		return arrString.substring(1, arrString.length()-1);
+	}
+
+}
